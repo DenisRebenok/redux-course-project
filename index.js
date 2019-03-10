f9770ebeb651e784ff7a1b012587c7274dd65fb4
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-03-10 20:32:34 +0200

    Leveraging Constants

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-// Reducer function
-function todos(state = [], action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return state.concat([action.todo]);
-    case 'REMOVE_TODO':
-      return state.filter(todo => todo.id !== action.id);
-    case 'TOGGLE_TODO':
-      return state.map(todo =>
-        todo.id !== action.id ? todo : { ...todo, complete: !todo.complete }
-      );
-    default:
-      return state;
-  }
-}
-
-function goals(state = [], action) {
-  switch (action.type) {
-    case 'ADD_GOAL':
-      return state.concat([action.goal]);
-    case 'REMOVE_GOAL':
-      return state.filter(goal => goal.id !== action.id);
-    default:
-      return state;
-  }
-}
-
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action)
-  };
-}
-
+// Library code
 function createStore(reducer) {
   // The store should have four parts
   // 1. The state
@@ -62,6 +29,47 @@ function createStore(reducer) {
     getState,
     subscribe,
     dispatch
+  };
+}
+
+// App code
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+// Reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter(todo => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map(todo =>
+        todo.id !== action.id ? todo : { ...todo, complete: !todo.complete }
+      );
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
   };
 }
 
